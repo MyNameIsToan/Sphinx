3bedcb29a6c0447e095c277850ae7142a4a65110
MyNameIsToan <86874910+MyNameIsToan@users.noreply.github.com>
2023-06-14 15:10:53 +0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -29,18 +29,11 @@ html_theme = 'alabaster'
 html_static_path = ['_static']
 
 needs_types = [
-               dict(directive="stkh_req", title="Stakeholder Requirement", prefix="STAKEHOLDER_REQ", color="#ac6dd1", style="artifact"),
                dict(directive="sys_req", title="System Requirement", prefix="SYSTEM_REQ_", color="#ac6dd1", style="artifact"),
                dict(directive="sw_req", title="Software Requirement", prefix="SOFTWARE_REQ_", color="#ac6dd1", style="artifact"),
-               dict(directive="interface_req", title="Interface Requirement", prefix="INTERFACE_REQ_", color="#ac6dd1", style="artifact"),
-               dict(directive="req_desc", title="Requirement Description", prefix="REQ_DESC_", color="#ac6dd1", style="artifact"),
-               dict(directive="verify", title="Verification Criteria", prefix="VC_", color="#fedcd2", style="artifact"),
-               dict(directive="design", title="Design", prefix="DESIGN_", color="ffcc00", style="artifact"),
-               dict(directive="design_decision", title="Design Decision", prefix="DESIGN_DECISION_", color="ffcc00", style="artifact"),
-               dict(directive="test_set", title="Test Set", prefix="TS_", color="#FEDCD2", style="artifact"),
-               dict(directive="test_case", title="Test Case", prefix="TC_", color="#bbff00", style="artifact"),
-               dict(directive="test_step", title="Test Step", prefix="TS_", color="#bbff00", style="artifact"),
-               dict(directive="test_run", title="Test Run", prefix="TR_", color="#cccccc", style="artifact"),
-               dict(directive="test_exe", title="Test Execution", prefix="TEX_", color="#bbff00", style="artifact"),
-               dict(directive="sw_comp", title="Software Component", prefix="SW_COMP_", color="#bbff00", style="artifact")
+               dict(directive="verify", title="Verification Criteria", prefix="VC_", color="#fedcd2", style="artifact")             
                ]
+
+needs_extra_options = ['created_by', 'url',  'date', 'time',  'safety_level',  'artifact_type', 'crq',
+                        'test_level']
+
